b18fb97c401cf41dfea00883f441643259e4199e
Tsion Adefres <tadefres@gmail.com>
2019-01-22 17:36:59 -0800

    Adding table header and styling components

---
+++ src/StyledPanel.js
@@ -0,0 +1,86 @@
+import { lighten, fade } from "@material-ui/core/styles/colorManipulator";
+
+const styledPanel = theme => ({
+  root: {
+    paddingRight: theme.spacing.unit,
+    width: '100%',
+  },
+  
+  highlight:
+    theme.palette.type === "light"
+      ? {
+        color: theme.palette.text.secondary,
+        backgroundColor: lighten(theme.palette.secondary.main, 0.74)
+        }
+      : {
+          color: theme.palette.text.secondary,
+          backgroundColor: theme.palette.primary.dark
+        },
+   
+  spacer: {
+    flex: "1 1 100%"
+  },
+  actions: {
+    paddingLeft: theme.spacing.unit,
+    color: theme.palette.text.primary,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+
+  title: {
+    flex: "0 0 auto",
+    fontSize: 25,
+    paddingLeft: theme.spacing.unit,
+    color: theme.palette.text.primary,
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.35),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.30),
+    },
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+
+  searchIcon: {
+    width: theme.spacing.unit * 10,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  inputRoot: {
+    color: theme.palette.text.primary,
+    width: '100%',
+  },
+  
+  inputInput: {
+    fontSize: 16,
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+});
+
+export default styledPanel;
---
+++ src/TableHeader.js
@@ -0,0 +1,89 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Tooltip from "@material-ui/core/Tooltip";
+
+const headers = [
+  {
+    index: "name",
+    label: "Account Name"
+  },
+  {
+    index: "country",
+    label: "Location"
+  },
+  {
+    index: "public",
+    label: "Publication Count"
+  },
+  {
+    index: "author",
+    label: "Author Count"
+  },
+  {
+    index: "type",
+    label: "Account Type"
+  },
+  {
+    index: "tags",
+    label: "Tags"
+  }
+];
+
+
+class TableHeader extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const {
+      order,
+      orderBy,
+    } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          {headers.map(
+            row => (
+              <TableCell 
+                key={row.index}
+                align={row.numeric ? "right" : "left"}
+                sortDirection={orderBy === row.index ? order : false}
+                style={{ fontSize: 16  }}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? "bottom-end" : "bottom-start"}
+                  enterDelay={200}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.index}
+                    direction={order}
+                    onClick={this.createSortHandler(row.index)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            ),
+            this
+          )}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+TableHeader.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+};
+
+export default TableHeader;
